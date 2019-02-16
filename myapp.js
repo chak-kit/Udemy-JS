6bf63cb0d7022ac27f8ddf0cfed6ae91ed8b4c2a
chak-kit <kitchakk93@gmail.com>
2019-02-16 18:21:41 +0200

    http

--- myapp.js
+++ myapp.js
@@ -5,19 +5,56 @@ var searchCity = document.querySelector('#city');
 
 searchButton.addEventListener('click', searchWeather);
 
-function searchWeather(){
-    var cityName = searchCity.value;
-    if (cityName.trim().length === 0){
- return alert ("Please enter the City Name")
-    }
-        let xhr = new XMLHttpRequest();
-        var apiKey ='059cf2b6f2741f27e2b1644ca23d139d';
-        var open =('GET', 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName);
-        xhr.onreadyStatechange = function () {
-            if (xhr.readyState == XMLHttpRequest.DONE && xtr.status===200){
+let load = document.querySelector("#load");
+let weatherBox = document.querySelector("#weather");
+let weatherCity = weatherBox.firstElementChild;
+let weatherDescription = document.querySelector('#weatherDescription');
+let weatherTemperature = weatherBox.lastElementChild;
 
-            } else if( xhr.readyState === XMLHttpRequest.DONE){
-                alert('Something went wrong')
-            }
-        }
+
+function searchWeather() {
+
+    weatherBox.style.display="none";
+    load.style.display='block';
+
+    var cityName = searchCity.value;
+    if (cityName.trim().length === 0) {
+        return alert("Please enter the City Name")
     }
+
+    let http = new XMLHttpRequest();
+    var apiKey = '059cf2b6f2741f27e2b1644ca23d139d';
+    http.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&apiKey=' + apiKey);
+    http.onload = function () {
+
+        if (http.readyState === XMLHttpRequest.DONE && http.status === 200) {
+            let data = JSON.parse(http.responseText);
+            let description = data.weather[0].description.toUpperCase();
+            let temp = data.main.temp;
+            let weather = new Weather(cityName, description);
+            weather.temperature = temp;
+            console.log(weather);
+
+            setTimeout(function () {
+                updateWeather(weather);
+            },2000);
+
+
+        } else if (http.readyState === XMLHttpRequest.DONE) {
+            alert('Something went wrong')
+        }
+    };
+    http.send()
+}
+
+
+function updateWeather(weather) {
+    weatherCity.textContent = weather.cityName;
+    weatherDescription.textContent=weather.description;
+    weatherTemperature.textContent =weather.temperature;
+
+
+    weatherBox.style.display="block";
+    load.style.display='none';
+
+}
--- weather-data.js
+++ weather-data.js
@@ -3,14 +3,14 @@
 function Weather(cityName, description) {
     this.cityName = cityName;
     this.description = description;
-    this._temperature = '';
+    this._temperature = "";
 }
 
 Object.defineProperty(Weather.prototype, 'temperature', {
-    get: function() {
+    get: function () {
         return this._temperature;
     },
-    set: function(value) {
+    set: function (value) {
         this._temperature = (value * 1.8 + 32).toFixed(2) + 'F.';
     }
 });
