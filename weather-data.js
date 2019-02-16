7537a0d41b9de890fb0992dccbddf05d59a9dc62
chak-kit <kitchakk93@gmail.com>
2019-02-16 19:47:19 +0200

    class Weather

--- weather-data.js
+++ weather-data.js
@@ -1,16 +1,58 @@
 "use strict";
 
-function Weather(cityName, description) {
-    this.cityName = cityName;
-    this.description = description;
-    this._temperature = "";
-}
+class Weather{
 
-Object.defineProperty(Weather.prototype, 'temperature', {
-    get: function () {
+    constructor(cityName, description) {
+        this._temperature = "";
+        this._cityName = cityName;
+        this._description = description;
+    }
+
+    get cityName() {
+        return this._cityName;
+    }
+
+    set cityName(value) {
+        this._cityName = value;
+    }
+
+    get description() {
+        return this._description;
+    }
+
+    set description(value) {
+        this._description = value;
+    }
+
+    get temperature() {
         return this._temperature;
-    },
-    set: function (value) {
+    }
+
+    set temperature(value) {
         this._temperature = (value * 1.8 + 32).toFixed(2) + 'F.';
     }
-});
+}
+
+
+
+
+
+
+
+
+
+
+// function WeatherOldStyle(cityName, description) {
+//     this.cityName = cityName;
+//     this.description = description;
+//     this._temperature = "";
+// }
+//
+// Object.defineProperty(Weather.prototype, 'temperature', {
+//     get: function () {
+//         return this._temperature;
+//     },
+//     set: function (value) {
+//         this._temperature = (value * 1.8 + 32).toFixed(2) + 'F.';
+//     }
+// });
